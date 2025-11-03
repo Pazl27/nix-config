4a9b6a9ca9add24780d00ce6a20acf627f12d799
Pazl <130466427+Pazl27@users.noreply.github.com>
2025-11-03 21:37:59 +0100

    treesitter updated

--- modules/home/editors/nvim/config/keymaps.nix
+++ modules/home/editors/nvim/config/keymaps.nix
@@ -178,7 +178,7 @@
     {
       mode = "i";
       key = "<C-v>";
-      action = "\"+p";
+      action = "<C-r>+";
       options = {
         desc = "Paste from system clipboard";
         silent = true;
