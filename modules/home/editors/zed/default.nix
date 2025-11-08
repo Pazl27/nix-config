0361de18a08ac0071575721bc92ceac586ea6b96
Pazl <130466427+Pazl27@users.noreply.github.com>
2025-11-08 13:40:55 +0100

    zed alias

--- modules/home/editors/zed/default.nix
+++ modules/home/editors/zed/default.nix
@@ -15,7 +15,7 @@ in
   options.features.editors.zed = {
     enable = mkEnableOption "Zed editor";
   };
-  
+
   config = mkIf config.features.editors.zed.enable {
     # Install Zed and dependencies
     home.packages = with pkgs; [
@@ -24,17 +24,17 @@ in
       vulkan-validation-layers
       vulkan-tools
       vulkan-extension-layer
-      
+
       # LSP servers
       rust-analyzer
       nil
       nixd
     ];
-    
+
     # Configure Zed
     programs.zed-editor = {
       enable = true;
-      
+
       # Apply settings with LSP configs
       userSettings = zedSettings // {
         lsp = {
@@ -48,13 +48,13 @@ in
               };
             };
           };
-          
+
           nil = {
             binary = {
               path = "${pkgs.nil}/bin/nil";
             };
           };
-          
+
           nixd = {
             binary = {
               path = "${pkgs.nixd}/bin/nixd";
@@ -62,24 +62,28 @@ in
           };
         };
       };
-      
+
       userKeymaps = zedKeybindings;
       userTasks = zedTasks;
-      
+
       extensions = [
         "nix"
       ];
     };
-    
+
     home.sessionVariables = {
       # Point to NVIDIA Vulkan driver
       VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
       VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
-      
+
       # Force NVIDIA
       __GLX_VENDOR_LIBRARY_NAME = "nvidia";
       GBM_BACKEND = "nvidia-drm";
       LIBVA_DRIVER_NAME = "nvidia";
+    };
+
+    programs.zsh.shellAliases = mkIf config.features.editors.zed.enable {
+      zed = "zeditor";
     };
   };
 }
