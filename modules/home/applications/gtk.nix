72f1d9deec5b7983560aaf2a309be7d0194338f1
Pazl <130466427+Pazl27@users.noreply.github.com>
2025-11-21 10:57:55 +0100

    nice looking theme

--- modules/home/applications/gtk.nix
+++ modules/home/applications/gtk.nix
@@ -14,23 +14,26 @@ with lib;
     gtk = {
       enable = true;
 
-      # theme = {
-      #   name = "Gruvbox-Dark";
-      #   package = pkgs.gruvbox-gtk-theme;
-      # };
-
       theme = {
         name = "Colloid-Red-Dark-Gruvbox";
-        package = pkgs.colloid-gtk-theme;
+        package = pkgs.colloid-gtk-theme.override {
+          themeVariants = [ "red" ]; # Farbe: red
+          colorVariants = [ "dark" ]; # Helligkeit: dark
+          sizeVariants = [ "standard" ]; # Größe: standard
+          tweaks = [ "gruvbox" ]; # Gruvbox palette!
+        };
       };
 
       iconTheme = {
         name = "Colloid-Red-Gruvbox-Dark";
-        package = pkgs.colloid-icon-theme;
+        package = pkgs.colloid-icon-theme.override {
+          schemeVariants = [ "gruvbox" ];
+          colorVariants = [ "red" ];
+        };
       };
 
       cursorTheme = {
-        name = "Gruvbox";
+        name = "Capitaine Cursors (Gruvbox)";
         package = pkgs.capitaine-cursors-themed;
         size = 24;
       };
@@ -42,37 +45,24 @@ with lib;
       };
 
       gtk3.extraConfig = {
-        gtk-application-prefer-dark-theme = true;
+        gtk-application-prefer-dark-theme = 1;
       };
 
       gtk4.extraConfig = {
-        gtk-application-prefer-dark-theme = true;
+        gtk-application-prefer-dark-theme = 1;
       };
     };
 
-    home.packages = with pkgs; [
-      gruvbox-gtk-theme
-      colloid-gtk-theme
-      colloid-icon-theme
-      bibata-cursors
-      capitaine-cursors-themed
-
-      nwg-look
-    ];
-
-    home.sessionVariables = {
-      # GTK_THEME = "Gruvbox-Dark";
-      XCURSOR_THEME = "Bibata-Modern-Classic";
-      XCURSOR_SIZE = "24";
-    };
-
     home.pointerCursor = {
-      name = "Gruvbox";
+      name = "Capitaine Cursors (Gruvbox)";
       package = pkgs.capitaine-cursors-themed;
       size = 24;
       gtk.enable = true;
       x11.enable = true;
     };
+
+    home.packages = with pkgs; [
+      nwg-look
+    ];
   };
 }
-
