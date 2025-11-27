70f5fe16fb323b18410336ab4807ba03133f9f2a
Pazl <130466427+Pazl27@users.noreply.github.com>
2025-11-27 14:43:24 +0100

    sshd var

--- hosts/desktop/variables.nix
+++ hosts/desktop/variables.nix
@@ -1,14 +1,19 @@
 {
   # ╔════════════════════════════════════════╗
-  # ║  BOOT CONFIGURATION                    ║
+  # ║  SYSTEM CONFIGURATIOe                  ║
   # ╚════════════════════════════════════════╝
   bootDevice = "nodev";
   useUEFI = true;
   sddmTheme = "japanese_aesthetic";
+  
+  sshAuthorizedKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII7akRGAUqpB2pTJ7jc8gVAhiOSYnxufSc5R3C+7RlHM nix-wsl@DE19352NB"
+  ];
 
   # ╔════════════════════════════════════════╗
-  # ║  GIT CONFIGURATION                     ║
+  # ║  USER CONFIGURATION                    ║
   # ╚════════════════════════════════════════╝
   gitUsername = "Pazl";
   gitEmail = "130466427+Pazl27@users.noreply.github.com";
+
 }
