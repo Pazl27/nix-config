d5be9ff78bb19a135cc9ce7f0cd37fb71ede7ebf
Pazl <130466427+Pazl27@users.noreply.github.com>
2026-01-07 19:59:12 +0100

    refactor: [#master] moved into zsh.nix

--- modules/home/tools/cli-tools.nix
+++ modules/home/tools/cli-tools.nix
@@ -1,4 +1,9 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 with lib;
 
@@ -10,18 +15,19 @@ with lib;
   config = mkIf config.features.tools.cliTools.enable {
     home.packages = with pkgs; [
       # Modern replacements
-      eza        # Better ls
-      ripgrep    # Better grep
-      fd         # Better find
-      fzf        # Fuzzy finder
-      zoxide     # Better cd
+      eza # Better ls
+      ripgrep # Better grep
+      fd # Better find
+      fzf # Fuzzy finder
+      zoxide # Better cd
+      serie
 
       # Utilities
       tree
       wget
       curl
-      jq         # JSON processor
-      yq-go      # YAML processor
+      jq # JSON processor
+      yq-go # YAML processor
       yazi
       gum
 
@@ -30,21 +36,7 @@ with lib;
       zip
 
       # Misc
-      tldr       # Simplified man pages
+      tldr # Simplified man pages
     ];
-
-    # Configure zoxide
-    programs.zoxide = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-
-    # Configure fzf
-    programs.fzf = {
-      enable = true;
-      enableZshIntegration = true;
-      defaultCommand = "${pkgs.fd}/bin/fd --type f";
-      fileWidgetCommand = "${pkgs.fd}/bin/fd --type f";
-    };
   };
 }
