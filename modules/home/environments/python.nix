f47b729e9d000c8a98726e2dec776a54bee0a00d
Pazl <130466427+Pazl27@users.noreply.github.com>
2026-01-08 18:21:54 +0100

    chore: [#master] format

--- modules/home/environments/python.nix
+++ modules/home/environments/python.nix
@@ -1,20 +1,22 @@
-{ config, lib, pkgs, ... }:
-
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 with lib;
-
 {
   options.features.environments.python = {
     enable = mkEnableOption "Python development environment";
   };
-
   config = mkIf config.features.environments.python.enable {
     home.packages = with pkgs; [
       python312
       python312Packages.pip
       python312Packages.virtualenv
-      python312Packages.poetry
-      ruff  # Fast Python linter
-      pyright  # Python LSP
+      poetry
+      ruff # Fast Python linter
+      pyright # Python LSP
     ];
   };
 }
