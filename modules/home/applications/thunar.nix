dc8af360380c893a582e24a7a9ec391c1c892ca2
Pazl <130466427+Pazl27@users.noreply.github.com>
2026-01-14 00:43:59 +0100

    chore: [#master] new package name

--- modules/home/applications/thunar.nix
+++ modules/home/applications/thunar.nix
@@ -12,16 +12,16 @@ with lib;
 
   config = mkIf config.features.application.thunar.enable {
     home.packages = with pkgs; [
-      xfce.thunar
-      xfce.thunar-volman
-      xfce.thunar-archive-plugin
-      xfce.thunar-media-tags-plugin
+      thunar
+      thunar-volman
+      thunar-archive-plugin
+      thunar-media-tags-plugin
 
       # File management
       file-roller
 
       # Thumbnails
-      xfce.tumbler
+      tumbler
       ffmpegthumbnailer
       libgsf
 
